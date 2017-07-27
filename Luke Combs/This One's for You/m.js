e39249f9e366a6c8770c79c2212c2082ff617e83
littleflute <13699175164@sina.cn>
2017-07-27 17:50:23 -0500

    Add files via upload

---
+++ Luke Combs/This One's for You/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Out There.mp3";		
+f[2]	= "02 Memories Are Made Of.mp3";		
+f[3]	= "03 Lonely One.mp3";		
+f[4]	= "04 Beer Can.mp3";		
+f[5]	= "06 One Number Away.mp3";		
+f[6]	= "07 Don't Tempt Me.mp3";		
+f[7]	= "08 When It Rains It Pours.mp3";		
+f[8]	= "09 This One's for You.mp3";		
+f[9]	= "10 Be Careful What You Wish For.mp3";		
+f[10]	= "11 I Got Away with You.mp3";		
+f[11]	= "12 Honky Tonk Highway.mp3";		
+f[12]	= "Hurricane.mp3";		
